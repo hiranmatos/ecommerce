b47f673c53c17179479cb89160c35503171796f3
hiranmatos <hiranmatos@outlook.com>
2018-08-09 00:11:19 -0300

    criada pageadmin

--- index.php
+++ index.php
@@ -2,13 +2,27 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	echo "OK";
+  
+	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+  
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
 
 });
 
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitbbca1600d8aee125a72648d0b9c50350
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitbbca1600d8aee125a72648d0b9c50350
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitbbca1600d8aee125a72648d0b9c50350::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitbbca1600d8aee125a72648d0b9c50350::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitbbca1600d8aee125a72648d0b9c50350::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitbbca1600d8aee125a72648d0b9c50350::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitbbca1600d8aee125a72648d0b9c50350::$classMap;
 
